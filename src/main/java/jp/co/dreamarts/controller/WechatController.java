ccc10e12b8b74a512706f3f402d0ba871c0bdd13
h_li <h_li@dreamats.co.jp>
2014-02-16 21:57:22 +0800

    接收用户

--- src/main/java/jp/co/dreamarts/controller/WechatController.java
+++ src/main/java/jp/co/dreamarts/controller/WechatController.java
@@ -10,17 +10,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WechatController {
 
 	@RequestMapping("/token.do")
-	public String testToken(HttpServletRequest request,HttpServletResponse response) {
+	public String testToken(HttpServletRequest request,
+			HttpServletResponse response) {
 
 		String signature = request.getParameter("signature");
 		String timestamp = request.getParameter("timestamp");
 		String nonce = request.getParameter("nonce");
 		String echostr = request.getParameter("echostr");
-		
+
 		String content = request.getParameter("Content");
 		String fromUserName = request.getParameter("FromUserName");
-		
-		
+
+		String username;
+		if (request.getAttribute("FromUserName") != null) {
+			username = (String) request.getAttribute("FromUserName");
+			System.out.println("username is " + username);
+		}
+
 		request.setAttribute("echostr", echostr);
 		System.out.println("signature is " + signature);
 		System.out.println("timestamp is " + timestamp);
@@ -28,8 +34,7 @@ public class WechatController {
 		System.out.println("echostr is " + echostr);
 		System.out.println("content is " + content);
 		System.out.println("fromUserName is " + fromUserName);
-		
+
 		return "index";
 	}
-
 }
